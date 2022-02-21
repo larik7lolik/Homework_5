db5304d334d9e00de29569f5cc72dbc5ff996146
LarisaZaripova <larik7lolik@gmail.com>
2022-02-21 15:31:48 +0300

    added Example 36

---
+++ Example_36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 36: Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечётных/чётных чисел.
+int[] arr = new int [10];
+int index = 0;
+int positive = 0;
+int negative = 0;
+void FillArray()
+{
+    for(int i = 0; i < arr.Length; i++)
+  {
+      arr[i] = new Random().Next(100, 1000);
+      Console.Write($"{arr[i]} ");
+  }
+}
+FillArray();
+while(index < arr.Length)
+{
+    if(arr[index] % 2 ==0)
+    {
+        positive++;
+    }
+    else
+    {
+      negative++;
+    }
+    index++;
+}
+Console.WriteLine();
+Console.Write($"Чётные числа {positive} ");
+Console.Write($"Нечётные числа {negative} ");
